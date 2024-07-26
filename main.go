f56ee93e846b25d48c9f42ccf05160febc41da25
l0wski <171383261+l0wski@users.noreply.github.com>
2024-07-25 20:40:25 -0500

    we do a little refactor

--- main.go
+++ main.go
@@ -13,9 +13,9 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func main() {
+const prefix string = "!lowski"
 
-	const prefix string = "!lowski"
+func main() {
 
 	// grab bot token
 	godotenv.Load()
@@ -27,35 +27,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	sess.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
-		// if message author is bot itself, do nothing
-		if m.Author.ID == s.State.User.ID {
-			return
-		}
-
-		args := strings.Split(m.Content, " ")
-
-		if args[0] != prefix {
-			return
-		}
-
-		// grab command
-		cmd := args[1]
-		switch {
-		case cmd == "help":
-			help := []string{
-				"you seem like you need help a lot",
-				"ever try helping yourself?",
-				"let me hold a dollar",
-				"lmao look at this guy asking for help",
-				"not feeling helpful fr",
-				"nah",
-				"can't do it",
-			}
-			choice := rand.Intn(len(help))
-			s.ChannelMessageSend(m.ChannelID, help[choice])
-		}
-	})
+	sess.AddHandler(messageCreate)
 
 	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged // hmm
 
@@ -65,11 +37,40 @@ func main() {
 	}
 	defer sess.Close() // close session after termination
 
-	fmt.Println("bot is online brother")
-
+	fmt.Println("bot is online brother.")
 
 	// keep bot running until os interrupt (CTRL + C)
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// if message author is bot itself, do nothing
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	args := strings.Split(m.Content, " ")
+
+	if args[0] != prefix {
+		return
+	}
+
+	// grab command
+	cmd := args[1]
+	switch {
+	case cmd == "help":
+		help := []string{
+			"you seem like you need help a lot",
+			"ever try helping yourself?",
+			"let me hold a dollar",
+			"lmao look at this guy asking for help",
+			"not feeling helpful fr",
+			"nah",
+			"can't do it",
+		}
+		choice := rand.Intn(len(help))
+		s.ChannelMessageSend(m.ChannelID, help[choice])
+	}
 }
